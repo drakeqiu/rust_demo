0b8782b1411c876e860636968c5fd32e2f9ac7a7
drakeqiu <adidasqiu@gmail.com>
2023-03-01 07:03:48 +0800

    code: 6-6

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    };
 }
