da6ce00885cdf6f796cbe5b12132e5ce5e846395
drakeqiu <adidasqiu@gmail.com>
2023-03-01 06:04:09 +0800

    code: 5-6

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        email: user1.email,
+        username: user1.username,
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
 }
