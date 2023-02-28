7bc8914589f7fd05592a924f4b800fafdf874b43
drakeqiu <adidasqiu@gmail.com>
2023-03-01 07:03:48 +0800

    code: 8-22

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 }
