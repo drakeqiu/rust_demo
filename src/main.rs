36e5acbbb7586357813a709851ca6ff6e906a02a
drakeqiu <adidasqiu@gmail.com>
2023-03-01 07:03:48 +0800

    code: 8-14

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let hello = String::from(" ");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from(" ");
+    let hello = String::from(
+        "
+",
+    );
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from(
+        "你好
+",
+    );
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
 }
