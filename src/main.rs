c3894beae671001c56b5b16b1415c689090f0946
drakeqiu <adidasqiu@gmail.com>
2023-03-01 06:04:09 +0800

    code: 5-5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        username,
+        email,
+        sign_in_count: 1,
+        active: true,
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user1.email = String::from("anotheremail@example.com");
 }
