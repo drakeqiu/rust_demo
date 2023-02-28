53fe5960b40646418fbe8e986674c784973b185b
drakeqiu <adidasqiu@gmail.com>
2023-03-01 06:04:09 +0800

    code: 4-8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    // s.clear();
+    println!("The first word is {}", word);
 }
