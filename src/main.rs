99fa80138f374547411c8d488873ac6660758fa7
drakeqiu <adidasqiu@gmail.com>
2023-03-01 07:03:48 +0800

    code: 8-20

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }
