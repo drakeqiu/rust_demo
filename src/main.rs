a3374d15791cfb3e43dfe43c3fbe934491459e95
drakeqiu <adidasqiu@gmail.com>
2023-03-01 07:03:48 +0800

    code: 8-21

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 }
