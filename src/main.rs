18e0632ba88961b1e791824bea8a33fec538d0a7
drakeqiu <adidasqiu@gmail.com>
2023-04-25 05:56:36 +0800

    code: 10-16

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-fn largest<T>(list: &[T]) -> &T
-where
-    T: PartialOrd,
-{
-    let mut largest = &list[0];
+use std::fmt::Display;
 
-    for item in list.iter() {
-        if item > largest {
-            largest = &item;
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T> Pair<T>
+where
+    T: Display + PartialOrd,
+{
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
         }
     }
-
-    largest
 }
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
-}
+fn main() {}
